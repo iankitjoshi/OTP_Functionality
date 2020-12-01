bb51a13f9842b8a4883086121e783a9d48d68dce
ankit-techindustan <ankitjoshi-techindustan@gmail.com>
2020-12-01 12:56:42 +0530

    added server configuration for react build

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const setUpDB = require('./config/database')
 const router = require('./config/routes')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 setUpDB()
@@ -10,11 +11,17 @@ const port = 3010
 
 app.use(express.json())
 app.use(cors())
+
+app.use(express.static(path.join("client", "build")))
+
 app.get('/',(req,res) => {
     res.json('Welcome to Kisan Networking')
 })
 app.use('/',router)
 
+app.get("/*", (req, res) => {
+    res.sendFile(path.join("client", "build", "index.html"))
+})
 
 app.listen(port , () => {
     console.log('Listening on port',port)
